2f9e5dc0f128c5716c00aea11d7a9e1a5950cf20
jorge <jorge@hotmail.com>
2019-04-29 23:52:13 -0500

    xd

---
+++ SA_TS.py
@@ -0,0 +1,102 @@
+from datetime import datetime
+from random import random
+
+class Avion :
+    def __init__ (self,idAvion,reg_number,icao,tipoAvion,tAerolinea):
+        self.idAvion= idAvion
+        self.regNumber = reg_number
+        self.icao = icao
+        self.tipoAvion = tipoAvion
+        self.tAerolinea = tAerolinea
+
+class TAerolinea:
+    def __init__ (self, idAerolinea, iata,icao):
+        self.idAerolinea = idAerolinea
+        self.iata = iata
+        self.icao = icao
+
+class TipoAvion:
+    def __init__(self, idTipoAvion,regNumber,capacidad,largo,ancho): 
+        self.idTipoAvion = idTipoAvion
+        self.regNumber = regNumber
+        self.capacidad=capacidad
+        self.largo=largo
+        self.ancho=ancho
+
+class Vuelo:
+    def __init__ (self,llegada,avion,horaEstimada,fechaEstimada, \
+        horaProgramada,fechaProgramada, icao, iata, \
+        numeroVuelo,estaEnTierra,latitud,longitud, \
+        altura, direccion, velocidadHorizontal, velocidadVertical):
+        self.horaEstimada = horaEstimada
+        self.fechaEstimada = fechaEstimada
+        self.horaProgramada = horaProgramada
+        self.fechaProgramada = fechaProgramada
+        self.icao = icao
+        self.iata = iata
+        self.numeroVuelo = numeroVuelo
+        self.estaEnTierra = estaEnTierra
+        self.latitud = latitud
+        self.longitud = longitud
+        self.altura = altura
+        self.direccion = direccion
+        self.velocidadHorizontal = velocidadHorizontal
+        self.velocidadVertical = velocidadVertical
+        self.asignado = False
+        self.area= None
+        self.avion = avion
+
+class BloqueVuelo:
+    def __init__(self):
+        self.vuelo = None
+        self.ocupado = None
+        self.sig = None
+        self.ant = None
+
+    def addVuelo(self,vuelo):
+        self.vuelo = vuelo
+        self.ocupado = True
+
+class listaVuelos:
+    def __init__ (self):
+        self.inicio = None
+        self.fin = None
+        self.cantidad=0
+    
+    def insertarBloque (self, bloque, pos=0):
+        if(self.inicio  is None):
+            self.inicio = bloque
+            self.fin = bloque
+        else:
+            self.inicio.sig.ant=bloque
+            self.inicio.sig = bloque
+            
+        self.cantidad += 1
+
+class Area :
+    def __init__ (self, idArea,  largo,ancho, coordenadaXCentro, coordenadaYCentro):
+        self.idPuerta = idArea
+        self.largo = largo
+        self.ancho = ancho
+        self.tiempoDisponible = datetime.now()
+        self.Vuelos = None #listaVuelos
+
+class Zona :
+    def __init__ (self, idZona, largo,ancho):
+        self.idZona = idZona
+        self.largo = largo
+        self.ancho=ancho
+
+class Manga: 
+    def __init__(self, puerta, longitud):
+        self.Puerta = puerta
+
+def main():
+    listaPuertas = []
+    for i  in range(10):
+        listaPuertas.append(Puerta(i,int(round((random()+1)*400)),(random()+1)*10,(random()+1)*10))
+    print (listaPuertas[2].capacidad)
+    #print(str(p.ancho)+" / "+str(p.capacidad) +" / " + str(p.tiempoDisponible))
+
+if __name__ == "__main__":
+    main()
